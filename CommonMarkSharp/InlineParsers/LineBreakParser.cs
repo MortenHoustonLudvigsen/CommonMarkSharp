208374cf3c80bbea3810058f53f27bd2f79a2502
Morten Houston Ludvigsen <mlu@2ps.dk>
2014-09-13 16:02:56 +0200

    Use Regex in stead of string

--- CommonMarkSharp/InlineParsers/LineBreakParser.cs
+++ CommonMarkSharp/InlineParsers/LineBreakParser.cs
@@ -4,12 +4,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CommonMarkSharp.InlineParsers
 {
     public class LineBreakParser : IParser<LineBreak>
     {
+        private static readonly Regex _startsWithDoubleSpaceNewLine = RegexUtils.Create(@"\G  +\n");
         public string StartsWithChars { get { return "\\ \n"; } }
 
         public LineBreak Parse(ParserContext context, Subject subject)
@@ -17,19 +19,19 @@ namespace CommonMarkSharp.InlineParsers
             if (!this.CanParse(subject)) return null;
 
             string[] groups;
-            if (subject.IsMatch(@"  +\n", 0, out groups))
+            if (subject.IsMatch(_startsWithDoubleSpaceNewLine, 0, out groups))
             {
                 subject.Advance(groups[0].Length);
                 subject.AdvanceWhile(c => c == ' ');
                 return new HardBreak();
             }
-            else if (subject.StartsWith("\\\n", 0))
+            else if (subject.StartsWith("\\\n"))
             {
                 subject.Advance(2);
                 subject.AdvanceWhile(c => c == ' ');
                 return new HardBreak();
             }
-            else if (subject.StartsWith(" \n", 0))
+            else if (subject.StartsWith(" \n"))
             {
                 subject.Advance(2);
                 subject.AdvanceWhile(c => c == ' ');
